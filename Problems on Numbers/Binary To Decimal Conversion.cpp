55d05915f0c27db45452187a716fba0f7055fc01
rammewada <sitarammewada077@gmail.com>
2026-03-10 18:51:37 +0530

    Binary to Decimal Conversion

--- Problems on Numbers/Binary To Decimal Conversion.cpp
+++ Problems on Numbers/Binary To Decimal Conversion.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 
+//Input - 10101001
+//Output - 169
 
 int BinaryToDecimal(int num){
-                                                          
+    int temp = num;
+    int decimalNumber=0; 
+    int  base=0;
+    while(temp){
+        int rem=temp%10;
+        temp=temp/10;
+
+        decimalNumber+=rem*pow(2,base);
+        base++;
+    }
+    return decimalNumber;
 }
 
 
@@ -12,5 +24,6 @@ int main () {
 
     int number;
     cin>>number;
-
+    cout<<BinaryToDecimal(number);
+  return 0;
 }
