bf6fcfc003403193c287173d95cf2183ee0da084
Sitaram Mewada <sitarammewada077@gmail.com>
2026-03-15 13:26:40 +0530

    merge the code

--- Array/demo.cpp
+++ Array/demo.cpp
@@ -1,11 +1,21 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> 
 using namespace std;
 
 
 
 
 int main() {
-    int x;
-    cin >> x;
-    cout << " Hey " << x;
-}  
+    double num ;
+    // getline(cin,str); 
+    // stringstream ss(str);
+    // vector<int> arr; int nums;
+
+    // while(ss>>nums) arr.push_back(nums);
+
+    //  for(auto x:arr) cout<<x<<"*"; 
+
+    cin>>num;
+    cout<<fixed<<setprecision(2)<<num;
+
+     return 0;
+} 
--- Array/find_the_smallest_number_in_an_array.cpp
+++ Array/find_the_smallest_number_in_an_array.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 int main (){
-    int arr[10]={2,34,5,12,57,44,31,9,3}; 
+
+    int arr[10]={2,34,5,12,57,1,44,31,9,3}; 
+
     int n=sizeof(arr)/sizeof(arr[0]);
     int smallest=INT_MAX;
     for(int i=0;i<n;i++){
